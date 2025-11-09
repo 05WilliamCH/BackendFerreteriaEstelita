4c2508bfd91dd49b8ef26f511b4cadfafe36f39c
05WilliamCH <wchiroym@miumg.edu.gt>
2025-11-09 01:14:09 -0600

    config.js eleiminado

--- src/db.js
+++ src/db.js
@@ -1,3 +1,15 @@
+// const {config} = require('dotenv')
+// config()
+
+// module.exports = {
+//     db: {
+//         user: process.env.USER,
+//         password: process.env.PASSWORD,
+//         host: process.env.HOST,
+//         port: process.env.PORT_DB,
+//         database: process.env.DATABASE
+//     }
+// } //config.js
 // const { Pool } = require("pg");
 // require("dotenv").config();
 
